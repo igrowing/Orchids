580d8ca0de6598176f6eea5e620bbda47847e3ae
igor <igrowing@gmail.com>
2017-03-04 19:54:09 +0200

    Some code cleanup.

--- orchid_app/management/commands/runner.py
+++ orchid_app/management/commands/runner.py
@@ -10,15 +10,12 @@ import time
 from threading import Thread
 
 from decimal import Decimal
-#from django.template.defaultfilters import floatformat
 from django.core.management.base import BaseCommand, CommandError
 from django.db import transaction
 
 from orchid_app import models
 from orchid_app.models import Sensors
 
-# from django.db import models
-# from django.urls import reverse
 from django.conf import settings
 from django.utils.six import python_2_unicode_compatible
 import paho.mqtt.subscribe as subscribe
@@ -29,8 +26,8 @@ from datetime import datetime
 POLL_PERIOD = 600  # seconds = 10 minutes
 POLL_PERIOD_MIN = POLL_PERIOD / 60  # minutes
 
-water_data = []
-wind_data = []
+water_data = [0.0]
+wind_data = [0.0]
 
 
 def runWater():
@@ -39,7 +36,6 @@ def runWater():
     while True:
         water_data = (subscribe.simple("shm/orchid/water/last_min").payload, time.time() - t < 120)
         t = time.time()
-        print 'Water:', water_data
 
 
 def runWind():
@@ -48,10 +44,7 @@ def runWind():
     while True:
         wind_data = (subscribe.simple("shm/orchid/wind/last_min").payload, time.time() - t < 120)
         t = time.time()
-        print 'Wind:', wind_data
 
-def get_payload(client, userdata, message):
-    return message.payload
 
 class Command(BaseCommand):
     help = 'Polls sensors and writes data into the DB.'
@@ -62,9 +55,6 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         # Run measure and publishing of GPIO data in background.
-        # threads = [Thread(target=anemometer.run),
-        #            Thread(target=water.run),
-        #            ]
         threads = [Thread(target=runWater),
                    Thread(target=runWind),
                    ]
@@ -76,23 +66,15 @@ class Command(BaseCommand):
             t = time.time()
             # Read i2c sensors
             s = Sensors()
+            # TODO: Add collection of 10 minutes per minute + averaging. Collect more, save less.
             n = datetime.now()
             s.date = n.replace(minute=n.minute / POLL_PERIOD_MIN * POLL_PERIOD_MIN, second=0, microsecond=0)  # reduce to poll period resolution
             s.t_amb, s.hpa, s.rh = bme.readBME280All()
             s.t_obj = mlx.Melexis().readObject1()
             s.lux = light.readLight()
-            self.stdout.write("Start handle")
-            # s.water = subscribe.callback(get_payload, "shm/orchid/water/last_min")  # read from MQTT
-            # self.stdout.write("Start ha")
-            # s.wind = subscribe.callback(get_payload, "shm/orchid/wind/last_min")
-            # s.water = subscribe.simple("shm/orchid/water/last_min").payload  # read from MQTT
-            # s.wind = subscribe.simple("shm/orchid/wind/last_min").payload
-            s.water = water_data[0] if water_data else 0.0
-            s.wind = wind_data[0] if wind_data else 0.0
+            s.water = water_data[0]
+            s.wind = wind_data[0]
 
-            import decimal
-            decimal.getcontext().prec = 1
-            self.stdout.write("Start postprocess" + str(water_data + wind_data))
             # Data conditioning by model/DB requirements
             s.t_amb = Decimal('{:.1f}'.format(s.t_amb))
             s.t_obj = Decimal('{:.1f}'.format(s.t_obj))
@@ -105,9 +87,8 @@ class Command(BaseCommand):
 
             # Write data to the DB
             s.save()
-            self.stdout.write('Redords: ' + repr(Sensors.objects.count()))
+            self.stdout.write('Records: ' + repr(Sensors.objects.count()))
             dt = 1.0 * POLL_PERIOD - (time.time() - t)
-            self.stdout.write("Sleeping... " + str(dt) + str(water_data))
             time.sleep(dt)
 
             # Example of catch bad data
