73f1f0bc39d7ea9db634bd27ed49763af409b67f
igor <igrowing@gmail.com>
2017-03-19 23:20:56 +0200

    Shut off actuators on system start added.

--- orchid_app/management/commands/runner.py
+++ orchid_app/management/commands/runner.py
@@ -2,46 +2,59 @@
 from __future__ import unicode_literals
 
 import orchid_app.sensors.anemometer as anemometer
-import orchid_app.sensors.yf_201s as water
 import orchid_app.sensors.max44009 as light
+import orchid_app.sensors.yf_201s as water
 import orchid_app.sensors.mlx90614 as mlx
 import orchid_app.sensors.bme280 as bme
-import time
-from threading import Thread
-
-from decimal import Decimal
-from django.core.management.base import BaseCommand, CommandError
-from django.db import transaction
-
-from orchid_app import models
-from orchid_app.models import Sensors
-
-from django.conf import settings
-from django.utils.six import python_2_unicode_compatible
 import paho.mqtt.subscribe as subscribe
-
 from datetime import datetime
+from threading import Thread
+from decimal import Decimal
+import time
+import os
 
+from django.core.management.base import BaseCommand, CommandError
+from django.utils.six import python_2_unicode_compatible
+from django.db import transaction
+from django.conf import settings
+
+#from orchid_app import models
+from orchid_app.models import Sensors
+from orchid_app.models import Actions
 
 POLL_PERIOD = 600  # seconds = 10 minutes
 POLL_PERIOD_MIN = POLL_PERIOD / 60  # minutes
 
-# water_data = []
-# wind_data = []
-#
-#
-# def run_water():
-#     water.run()
-#     # while True:
-#     #     global water_data
-#     #     water_data.append(subscribe.simple("shm/orchid/water/last_min").payload)
-#
-#
-# def run_wind():
-#     anemometer.run()
-#     # while True:
-#     #     global wind_data
-#     #     wind_data.append(subscribe.simple("shm/orchid/wind/last_min").payload)
+def on_start(self):
+    '''Shut down everything could be open.'''
+    import orchid_app.actuators.latching_valves as valves
+    import orchid_app.actuators.valve as relays
+
+    rep = []
+    a = Actions()
+    a.date = datetime.now()
+    try:
+        valves.LatchingValve(1).set_status(0)
+        rep.append('LatchingValve:water(1)')
+        a.water = False
+        valves.LatchingValve(2).set_status(0)
+        rep.append('LatchingValve:mist(2)')
+        a.mist = False
+        relays.Relay(1).set_status(0)
+        rep.append('Relay:fan(1)')
+        a.fan = False
+        relays.Relay(2).set_status(0)
+        rep.append('Relay:light(2)')
+        a.light = False
+
+        os.system('logger orchid_runner: shut off all actuators: ' + ', '.join(rep))
+        a.save()
+    except Exception as e:
+        os.system('logger orchid_runner: failed to shut off all actuators. OK with: ' + ', '.join(rep))
+        self.stderr.write('On start: %s (%s)' % (e.message, type(e)))
+
+    self.stdout.write('Shut off all actuators: ' + ', '.join(rep))
+    self.stdout.write('Action Records: ' + repr(Actions.objects.count()))
 
 
 def avg(l):
@@ -70,8 +83,8 @@ class Command(BaseCommand):
             t.setDaemon(True)
             t.start()
 
-        import os
         os.system('logger orchid_runner has started')
+        on_start(self)
 
         data = {'wind': [], 'water': [], 't_amb': [], 't_obj': [], 'hpa': [], 'rh': [], 'lux': []}
         ts = time.time()
@@ -92,7 +105,7 @@ class Command(BaseCommand):
                     data['t_obj'].append(mlx.Melexis().readObject1())
                     data['lux'].append(light.readLight())
                 except Exception as e:
-                    self.stderr.write('%s (%s)' % (e.message, type(e)))
+                    self.stderr.write('On read: %s (%s)' % (e.message, type(e)))
                     time.sleep(60)  # Wait 1 minute before retry.
             else:
                 n = datetime.now()
@@ -110,9 +123,9 @@ class Command(BaseCommand):
                 try:  # Catch sensor reading data, stay running
                     # Write data to the DB
                     s.save()
-                    self.stdout.write('Records: ' + repr(Sensors.objects.count()))
+                    self.stdout.write('Sensor Records: ' + repr(Sensors.objects.count()))
                 except Exception as e:
-                    self.stderr.write('%s (%s)' % (e.message, type(e)))
+                    self.stderr.write('On write: %s (%s)' % (e.message, type(e)))
                     time.sleep(60)  # Wait 1 minute before retry.
                 # Reset the data structure
                 data = {'wind': [], 'water': [], 't_amb': [], 't_obj': [], 'hpa': [], 'rh': [], 'lux': []}
